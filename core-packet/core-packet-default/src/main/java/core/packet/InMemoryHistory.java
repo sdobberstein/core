81776f483a2cc07063c73832e02777f70206dbf5
Dan Bogert <dan.bogert@gmail.com>
2012-03-21 22:06:24 -0400

    removed generic from history

--- core-packet/core-packet-default/src/main/java/core/packet/InMemoryHistory.java
+++ core-packet/core-packet-default/src/main/java/core/packet/InMemoryHistory.java
@@ -5,17 +5,17 @@ import java.util.List;
 
 public class InMemoryHistory implements History {
 
-	private List<HistoryEvent<?>> history;
+	private List<HistoryEvent> history;
 	
 	public InMemoryHistory() {
-		history = new ArrayList<HistoryEvent<?>>();
+		history = new ArrayList<HistoryEvent>();
 	}
 	
-	public void addEvent(HistoryEvent<?> event) {
+	public void addEvent(HistoryEvent event) {
 		history.add(event);
 	}
 
-	public List<HistoryEvent<?>> getHistory() {
+	public List<HistoryEvent> getHistory() {
 		return history;
 	}
 }
